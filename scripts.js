4e0203ecd208ef81b7212732287cabb23aae0c43
Paulo Kinopk <pkinopk@gmail.com>
2017-04-13 23:52:17 -0700

    Completed Binary Agents

--- scripts.js
+++ scripts.js
@@ -366,3 +366,20 @@ steamrollArray([1, {},
 
 //------------------------------------------------------------------------------
 //Binary Agents
+
+function binaryAgent(str) {
+  var bin = str.split(" ");
+  var arr = [];
+  var result = "";
+
+  for (var i = 0; i < bin.length; i++) {
+    arr.push(parseInt(bin[i], 2));
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    result += String.fromCharCode(arr[i]);
+  }
+  return result;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
