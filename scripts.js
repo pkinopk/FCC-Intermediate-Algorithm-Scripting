b41a16ee4f653cab41fb2c3c1dc9e107b67c7eec
Paulo Kinopk <pkinopk@gmail.com>
2017-04-13 22:56:15 -0700

    Added Steamroller

--- scripts.js
+++ scripts.js
@@ -327,3 +327,6 @@ function dropElements(arr, func) {
 dropElements([0, 1, 0, 1], function(n) {
   return n === 1;
 });
+
+//------------------------------------------------------------------------------
+//Steamroller
